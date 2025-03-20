1fa5618262c4b6019f8461094720a67f09d0fd1d
Putra28 <suyapratama2217@gmail.com>
2025-03-20 11:56:55 +0700

    update list arabic font selection

--- lib/widget/ArabicFontsListModal_widget.dart
+++ lib/widget/ArabicFontsListModal_widget.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:provider/provider.dart';
 import '../provider/settings_provider.dart';
-import 'dart:convert';
-import 'package:flutter/services.dart';
 
 class SettingsWidget extends StatefulWidget {
   const SettingsWidget({Key? key}) : super(key: key);
@@ -12,34 +10,17 @@ class SettingsWidget extends StatefulWidget {
   State<SettingsWidget> createState() => _SettingsWidgetState();
 }
 
-
-
 class _SettingsWidgetState extends State<SettingsWidget> {
   int _arabicFontSize = 22;
   int _latinFontSize = 16;
   int _translationFontSize = 16;
   String _arabicFontFamily = 'ScheherazadeNew';
   FontWeight _arabicFontWeight = FontWeight.bold;
-  List<String> fonts = [];
-  List<String> filteredFonts = [];
-  bool isLoading = true;
-  String searchQuery = '';
 
   @override
   void initState() {
     super.initState();
     _loadSettings();
-    _loadFonts();
-  }
-
-  Future<void> _loadFonts() async {
-    final String response = await rootBundle.loadString('assets/json/fonts/fonts.json');
-    final data = json.decode(response);
-    setState(() {
-      fonts = List<String>.from(data["ArabicFonts"]);
-      filteredFonts = List.from(fonts);
-      isLoading = false;
-    });
   }
 
   _loadSettings() async {
@@ -48,9 +29,11 @@ class _SettingsWidgetState extends State<SettingsWidget> {
       _arabicFontSize = prefs.getInt('arabicFontSize') ?? 22;
       _latinFontSize = prefs.getInt('latinFontSize') ?? 16;
       _translationFontSize = prefs.getInt('translationFontSize') ?? 16;
-      _arabicFontFamily = prefs.getString('arabicFontFamily') ?? 'ScheherazadeNew';
+      _arabicFontFamily =
+          (prefs.getString('arabicFontFamily') ?? 'ScheherazadeNew');
       String weightString = prefs.getString('arabicFontWeight') ?? 'normal';
-      _arabicFontWeight = (weightString == 'bold') ? FontWeight.bold : FontWeight.normal;
+      _arabicFontWeight =
+          (weightString == 'bold') ? FontWeight.bold : FontWeight.normal;
     });
   }
 
@@ -65,98 +48,94 @@ class _SettingsWidgetState extends State<SettingsWidget> {
     await prefs.setString('arabicFontWeight', weightString);
   }
 
-  void _showFontSelection() {
+  _showFontSelection() {
     showModalBottomSheet(
       context: context,
       builder: (context) {
         final settings = Provider.of<SettingsProvider>(context);
-        return StatefulBuilder( // Tambahkan StatefulBuilder di sini
-          builder: (context, setModalState) {
-            return Container(
-              height: MediaQuery.of(context).size.height * 0.6,
-              padding: const EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: const BorderRadius.vertical(top: Radius.circular(20)),
+        return Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                title: Text('Scheherazade New'),
+                selected: settings.arabicFontFamily == 'Scheherazade New',
+                onTap: () {
+                  settings.setArabicFontFamily('Scheherazade New');
+                  setState(() {
+                    _arabicFontFamily = 'Scheherazade New';
+                  });
+                  _saveSettings();
+                  Navigator.pop(context); // Tutup modal
+                },
               ),
-              child: Column(
-                children: [
-                  Center(
-                    child: Container(
-                      width: 40,
-                      height: 5,
-                      margin: const EdgeInsets.only(bottom: 10),
-                      decoration: BoxDecoration(
-                        color: Colors.grey[400],
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 4),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.grey[200],
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 12),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: 'Search Fonts...',
-                            hintStyle: TextStyle(
-                              color: Colors.grey[400],
-                            ),
-                          ),
-                          onChanged: (query) {
-                            setModalState(() { // Gunakan setModalState agar UI modal terupdate
-                              searchQuery = query;
-                              if (query.isEmpty) {
-                                filteredFonts = List.from(fonts);
-                              } else {
-                                filteredFonts = fonts
-                                    .where((font) => font.toLowerCase().startsWith(query.toLowerCase()))
-                                    .toList();
-                              }
-                            });
-                          },
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  isLoading
-                      ? Expanded(child: Center(child: CircularProgressIndicator()))
-                      : Expanded(
-                          child: ListView.builder(
-                            itemCount: filteredFonts.length,
-                            itemBuilder: (context, index) {
-                              final font = filteredFonts[index];
-                              return ListTile(
-                                title: Text(font),
-                                selected: settings.arabicFontFamily == font,
-                                onTap: () {
-                                  settings.setArabicFontFamily(font);
-                                  setState(() {
-                                    _arabicFontFamily = font;
-                                  });
-                                  _saveSettings();
-                                  Navigator.pop(context);
-                                },
-                              );
-                            },
-                          ),
-                        ),
-                ],
+              ListTile(
+                title: Text('Amiri Quran'),
+                selected: settings.arabicFontFamily == 'Amiri Quran',
+                onTap: () {
+                  settings.setArabicFontFamily('Amiri Quran');
+                  setState(() {
+                    _arabicFontFamily = 'Amiri Quran';
+                  });
+                  _saveSettings();
+                  Navigator.pop(context); // Tutup modal
+                },
               ),
-            );
-          },
+              ListTile(
+                title: Text('Amiri'),
+                selected: settings.arabicFontFamily == 'Amiri',
+                onTap: () {
+                  settings.setArabicFontFamily('Amiri');
+                  setState(() {
+                    _arabicFontFamily = 'Amiri';
+                  });
+                  _saveSettings();
+                  Navigator.pop(context); // Tutup modal
+                },
+              ),
+              ListTile(
+                title: Text('Lateef'),
+                selected: settings.arabicFontFamily == 'Lateef',
+                onTap: () {
+                  settings.setArabicFontFamily('Lateef');
+                  setState(() {
+                    _arabicFontFamily = 'Lateef';
+                  });
+                  _saveSettings();
+                  Navigator.pop(context); // Tutup modal
+                },
+              ),
+              ListTile(
+                title: Text('Noto Naskh Arabic'),
+                selected: settings.arabicFontFamily == 'Noto Naskh Arabic',
+                onTap: () {
+                  settings.setArabicFontFamily('Noto Naskh Arabic');
+                  setState(() {
+                    _arabicFontFamily = 'Noto Naskh Arabic';
+                  });
+                  _saveSettings();
+                  Navigator.pop(context); // Tutup modal
+                },
+              ),
+              ListTile(
+                title: Text('Noto Nastaliq Urdu'),
+                selected: settings.arabicFontFamily == 'Noto Nastaliq Urdu',
+                onTap: () {
+                  settings.setArabicFontFamily('Noto Nastaliq Urdu');
+                  setState(() {
+                    _arabicFontFamily = 'Noto Nastaliq Urdu';
+                  });
+                  _saveSettings();
+                  Navigator.pop(context); // Tutup modal
+                },
+              ),
+            ],
+          ),
         );
       },
     );
   }
-
 
   _showFontWeightSelection() {
     showModalBottomSheet(
